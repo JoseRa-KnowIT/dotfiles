77d1e2833e0c1f05ebb19a10660b996c004ce3c1
José Ramón Aguilar <jraguilar@knowit.mx>
2025-11-14 18:20:14 -0600

    Add bootstrap.ps1

---
+++ pwsh/bootstrap.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$file = $PROFILE.CurrentUserAllHosts
+$content = '. $HOME/dotfiles/pwsh/profile.base.ps1'
+
+if(Test-Path $file) {
+    Write-Warning "Profile file '$file' already exists"
+    Write-Warning "Please add the following line to the current profile if needed: $content"
+}
+else {
+    Set-Content $file -Value $content
+    "Profile file created succesfully '$file'"
+}
+
+#TODO: Instalar modulos requeridos
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,1 +1,4 @@
 find ~/dotfiles/ -type f -name ".*" -exec ln -s -f {} ~ ';'
+
+#TODO: Configurar git en WSL
+# credential.helper=/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe
