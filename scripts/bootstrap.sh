7679f36be322c0729ddae56422e373e6e7f83765
José Ramón Aguilar <jraguilar@knowit.mx>
2026-09-08 22:30:36 -0600

    Add Hyprland configs

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -10,5 +10,19 @@ find ~/dotfiles/home/ -type f -name ".*" -exec sh -c '
     done
 ' sh {} +
 
+find ~/dotfiles/.config/ -type f -exec sh -c '
+    for src do
+        relative_path="${src#"$HOME/dotfiles/"}"
+        dst="$HOME/$relative_path"
+        mkdir -p "$(dirname "$dst")"
+
+        if [ -L "$dst" ]; then
+            ln -srf "$src" "$dst"
+        else
+            ln -srb "$src" "$dst"
+        fi
+    done
+' sh {} +
+
 #TODO: Configurar git en WSL
 # credential.helper=/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe
